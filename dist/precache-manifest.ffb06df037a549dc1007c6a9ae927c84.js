bb8177cfa33ff2cb6be70ea603302aae5dbf8fd3
mhdrahmoun <mrahmoun93@gmail.com>
2020-03-31 00:59:35 +0800

    staging ready build

---
+++ dist/precache-manifest.ffb06df037a549dc1007c6a9ae927c84.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aeee30662934313ab06",
+    "url": "/css/app.7ef45176.css"
+  },
+  {
+    "revision": "4ec60ed69440c5183604",
+    "url": "/css/chunk-vendors.723e60e8.css"
+  },
+  {
+    "revision": "2ec146b9481fc1864aed0cfc32f31588",
+    "url": "/fonts/Flaticon.2ec146b9.ttf"
+  },
+  {
+    "revision": "c33038e1046cfec2938186c8e2bf558a",
+    "url": "/fonts/Flaticon.c33038e1.eot"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/fonts/themify.2c454669.eot"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/fonts/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/fonts/themify.e23a7dca.ttf"
+  },
+  {
+    "revision": "43ce8c91146e2248954595b063627646",
+    "url": "/img/Flaticon.43ce8c91.svg"
+  },
+  {
+    "revision": "5727c440b51482d47217ed40ff4ad812",
+    "url": "/img/add_property.5727c440.png"
+  },
+  {
+    "revision": "c8e61c76bccd20f5d10838798a137f8d",
+    "url": "/img/banner.c8e61c76.png"
+  },
+  {
+    "revision": "6c1789bec3b03982fe06f1e8ffb9951f",
+    "url": "/img/banner_2.6c1789be.png"
+  },
+  {
+    "revision": "a12e0353d78d6cb07b1a43202b91a04c",
+    "url": "/img/bradcam.a12e0353.png"
+  },
+  {
+    "revision": "72b226aafa77debfd5512ae7b41c61b2",
+    "url": "/img/bradcam2.72b226aa.png"
+  },
+  {
+    "revision": "281d652261bf75e466b01f8a1bb3271f",
+    "url": "/img/bradcam3.281d6522.png"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "cd5f8093e9965e423cfbcf1c2c768d79",
+    "url": "/img/home_details.cd5f8093.png"
+  },
+  {
+    "revision": "adbccee0708ae3b7a71d9652fb353299",
+    "url": "/img/image.adbccee0.png"
+  },
+  {
+    "revision": "a2627b34209c4aa97a6b1c93251ef0f7",
+    "url": "/img/info_banner.a2627b34.png"
+  },
+  {
+    "revision": "a12e0353d78d6cb07b1a43202b91a04c",
+    "url": "/img/property.a12e0353.png"
+  },
+  {
+    "revision": "c884c1b6528e9a78eaa5ed475e356c1b",
+    "url": "/img/sprayed_bg.c884c1b6.png"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/img/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "0a16b604d2bfaa6d1b2225e435c7ad79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "075b34856a4dbc0cb3b3",
+    "url": "/js/about.4f06ce43.js"
+  },
+  {
+    "revision": "2aeee30662934313ab06",
+    "url": "/js/app.62987e96.js"
+  },
+  {
+    "revision": "4ec60ed69440c5183604",
+    "url": "/js/chunk-vendors.7230fbd0.js"
+  },
+  {
+    "revision": "599ac362afc6b369c7ea477ef161885d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
