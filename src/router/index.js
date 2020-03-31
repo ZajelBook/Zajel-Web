8c4490088e0ac47721be5eefd0f0400d4b9e124d
mhdrahmoun <mrahmoun93@gmail.com>
2020-04-01 01:06:39 +0800

    add policy and terms pages

--- src/router/index.js
+++ src/router/index.js
@@ -24,15 +24,26 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/policy',
+    name: 'Policy',
+    component: () => import(/* webpackChunkName: "about" */ '../views/PrivacyPolicy.vue')
+  },
+  {
+    path: '/terms',
+    name: 'Terms',
+    component: () => import(/* webpackChunkName: "about" */ '../views/TermsAndConditions.vue')
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history',
+  scrollBehavior() {
+    return { x: 0, y: 0 }
+  }
 })
 
 export default router
